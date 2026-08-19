ce24b705ca4a75ab4b787c7fd1d2e4bb70186e6a
ZainHaubsh <zhaubsh@gmail.com>
2026-08-19 22:30:32 +0300

    Add template pages

---
+++ EcommerceSystem/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceSystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
