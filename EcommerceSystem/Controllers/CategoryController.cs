dd069ac17dc8bde85ae00d8ba12c6ed0b7d5bb67
Bruceirshaidat <majedabbadi08@gmail.com>
2026-08-20 14:31:12 +0300

    add

---
+++ EcommerceSystem/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using EcommerceSystem.Data;
+using EcommerceSystem.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceSystem.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //get  category
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.Where(c => !c.IsDeleted).ToListAsync();
+
+            return View(categories);
+
+        }
+
+        // GET: Category/Details
+
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(c => c.CategoryId == id && !c.IsDeleted);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+
+        // GET: Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
