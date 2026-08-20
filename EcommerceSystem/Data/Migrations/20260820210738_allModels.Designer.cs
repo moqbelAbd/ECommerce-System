1e077be2d4ffefeeaa8a1a19c94629aa4aa22044
moqbelAbd <yabd390@gmail.com>
2026-08-21 00:09:15 +0300

    done models

--- EcommerceSystem/Data/Migrations/20260820210738_allModels.Designer.cs
+++ EcommerceSystem/Data/Migrations/20260820210738_allModels.Designer.cs
@@ -4,6 +4,7 @@ using EcommerceSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcommerceSystem.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260820210738_allModels")]
+    partial class allModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EcommerceSystem/Data/Migrations/20260820210738_allModels.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class allModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_OrderItem_OrderItemId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_OrderItemId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OrderItemId",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "CustomerPaymentCards",
+                columns: table => new
+                {
+                    PaymentCardId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CardExpire = table.Column<DateOnly>(type: "date", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerPaymentCards", x => x.PaymentCardId);
+                    table.ForeignKey(
+                        name: "FK_CustomerPaymentCards_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductReviews",
+                columns: table => new
+                {
+                    ProductReviewId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerProductRating = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerProductReview = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductReviews", x => x.ProductReviewId);
+                    table.ForeignKey(
+                        name: "FK_ProductReviews_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductReviews_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerPaymentCards_CustomerId",
+                table: "CustomerPaymentCards",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductReviews_CustomerId",
+                table: "ProductReviews",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductReviews_ProductId",
+                table: "ProductReviews",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Orders_ProductId",
+                table: "OrderItem",
+                column: "ProductId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Orders_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropTable(
+                name: "CustomerPaymentCards");
+
+            migrationBuilder.DropTable(
+                name: "ProductReviews");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderItemId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_OrderItemId",
+                table: "Products",
+                column: "OrderItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_OrderItem_OrderItemId",
+                table: "Products",
+                column: "OrderItemId",
+                principalTable: "OrderItem",
+                principalColumn: "OrderItemId");
+        }
+    }
+}
