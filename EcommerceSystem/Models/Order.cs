a7e4d68104afb3f9a00798be28c42dd8ac4b8ab5
moqbel <yabd390@gmail.com>
2026-08-20 08:36:23 +0300

    Added main tables

---
+++ EcommerceSystem/Models/Order.cs
@@ -0,0 +1,41 @@
+namespace EcommerceSystem.Models
+{
+
+public class Order{
+
+[Key]
+public Guid OrderId {get; set;}
+
+[Required]
+public decimal TotalPrice {get; set;}
+
+public DateTime CreatedAt { get; set; } = DateTime.Now; // Automatically sets the current time!
+
+[Required]
+public int orderStatus  {get; set;}
+
+//[ForeignKey("OrderStatusId")]
+//public OrderStatus? OrderStatus { get; set; }
+
+[Required]
+public int paymentStatus  {get; set;}
+
+//[ForeignKey("PaymentStatusId")]
+//public PaymentStatus? PaymentStatus { get; set; }
+
+[Required]
+public int paymentTypeId {get; set;}
+
+//[ForeignKey("PaymentTypeId")]
+//public PaymentType? PaymentType { get; set; }
+
+public Guid CustomerId {get; set;}
+
+[ForeignKey ("CustomerId")]
+public Customer? Customer {get; set;}
+
+//public ICollection<OrderItem> OrderItems {get; set;} = new HashSet<OrderItem>();
+
+}
+
+}
